efeb0e59f6d6dc9e8bc5ac1cd96b8a3bb6299fe4
Steve Davis <sdavis@four51.com>
2014-02-25 14:20:02 -0600

    hard hat product

--- app/js/controllers/hardHatCtrl.js
+++ app/js/controllers/hardHatCtrl.js
@@ -1,3 +1,13 @@
-/**
- * Created by SteveDavis on 2/25/14.
- */
+four51.app.controller('HardHatCtrl', function ($scope, Product, ProductDisplayService) {
+	$scope.LineItem = {};
+
+	ProductDisplayService.getProductAndVariant('HH',null, function(data){
+		$scope.LineItem.Product = data.product;
+		$scope.LineItem.Variant = data.variant;
+		ProductDisplayService.setNewLineItemScope($scope);
+		ProductDisplayService.setProductViewScope($scope);
+		$scope.$broadcast('ProductGetComplete');
+		$scope.loadingIndicator = false;
+		$scope.setAddToOrderErrors();
+	});
+});
--- app/js/routing.js
+++ app/js/routing.js
@@ -36,5 +36,6 @@ four51.app.config(function($routeProvider, $locationProvider) {
             when('/search/:searchTerm', { templateUrl: 'partials/searchView.html', controller: 'ProductSearchCtrl' }).
             when('/security', { templateUrl: 'partials/security.html', controller: 'SecurityCtrl' }).
             when('/conditions', { templateUrl: 'partials/conditions.html', controller: 'ConditionsCtrl' }).
-			otherwise({redirectTo: '/catalog'});
+			when('/hardhat', { templateUrl: 'partials/ProductViews/hardhat.html', controller: 'HardHatCtrl' }).
+			otherwise({redirectTo: '/hardhat'});
 	});
